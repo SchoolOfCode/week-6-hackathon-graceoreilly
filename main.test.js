70cb1346ac4f1e82f5e0801d3e03d797983d509a
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-01-16 19:46:16 +0000

    Test 2 and 3 added, working on test 4
    Test 2 added to handle arrays with null values
    Test 3 added to handle duplicates in an array
    Working on test 4 to handle case sensitivity

--- main.test.js
+++ main.test.js
@@ -1,17 +1,59 @@
 import { test, expect } from "vitest"
 import { removeMurdered } from "./main.js"
 
-// Plan
-// Test 1 - Create a function (removeMurdered) that takes in an array of faithfuls and one murdered faithful and returns the updated array without the murdered faithful.
-test("should remove the murdered faithful from the array", () => {
+
+// Test 1
+test("1 - should remove the murdered faithful from the array", () => {
     // Arrange
     // Variables - an array of faithfuls and a string representing the murdered faithful
-    const faithfuls = ["Leanne", "Livi", "Fozia", "Maia", "Jake"]
+    const faithfuls = ["Leanne", "Livi", "Tyler", "Kas", "Jake"]
     const murdered = "Livi"
 // Act
 // Pass the variables to the removedMurdered function with the array and the murdered faithful
 const result = removeMurdered(faithfuls, murdered)
 // Assert
 // Should return the array without the murdered faithful if the murdered is in the array
-expect(result).toStrictEqual(["Leanne", "Fozia", "Maia", "Jake"])
+expect(result).toStrictEqual(["Leanne", "Tyler", "Kas", "Jake"])
 });
+
+// Test 2
+test("2 - should handle arrays with null values", () => {
+    // Arrange
+    // Variables - an array of faithfuls and a string representing the murdered faithful
+    const faithfuls = ["Leanne", "Livi", "Tyler", null, "Kas", "Jake"]
+    const murdered = null
+    // Act
+    // Pass the variables to the removedMurdered function with the array and the murdered
+    const result = removeMurdered(faithfuls, murdered)
+    // Assert
+    // Should return the array without the null value
+    expect(result).toStrictEqual(["Leanne", "Livi", "Tyler", "Kas", "Jake"])
+});
+
+// Test 3
+test("3 - should handle duplicates in an array", () => {
+    // Arrange
+    // Variables - an array of faithfuls and a string representing the murdered faithful
+    const faithfuls = ["Leanne", "Livi", "Tyler", "Kas", "Jake", "Livi"]
+    const murdered = "Livi"
+    // Act
+    // Pass the variables to the removedMurdered function with the array and the murdered
+    const result = removeMurdered(faithfuls, murdered)
+    // Assert
+    // Should return the array without the murdered faithful if the murdered is in the array
+    expect(result).toStrictEqual(["Leanne", "Tyler", "Kas", "Jake"])
+});
+
+// Test 4
+// test("4 - should be case sensitive", () => {
+//     // Arrange
+//     // Variables - an array of faithfuls and a string representing the murdered faithful
+//     const faithfuls = ["Leanne", "LIVI", "Tyler", "Kas", "Jake"]
+//     const murdered = "livi"
+//     // Act
+//     // Pass the variables to the removedMurdered function with the array and the murdered
+//     const result = removeMurdered(faithfuls, murdered)
+//     // Assert
+//     // Should return the array without the murdered faithful if the murdered is in the array
+//     expect(result).toStrictEqual(["Leanne", "Tyler", "Kas", "Jake"])
+// });
